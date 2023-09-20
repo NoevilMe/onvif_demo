b7ca44bd68c0007481c447ce8cabf56cbaa7e802
Jason.Dunn <surpass168@live.com>
2023-09-20 17:23:38 +0800

    update scan_device

--- tests/scan_device.cpp
+++ tests/scan_device.cpp
@@ -10,14 +10,6 @@
 #define SOAP_DBGLOG printf
 #define SOAP_DBGERR printf
 
-#define SOAP_TO "urn:schemas-xmlsoap-org:ws:2005:04:discovery"
-#define SOAP_ACTION "http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe"
-
-#define SOAP_MCAST_ADDR "soap.udp://239.255.255.250:3702" // onvif规定的组播地址
-
-#define SOAP_ITEM ""                            // 寻找的设备范围
-#define SOAP_TYPES "dn:NetworkVideoTransmitter" // 寻找的设备类型
-
 #define SOAP_SOCK_TIMEOUT (10) // socket超时时间（单秒秒）
 
 #define SOAP_CHECK_ERROR(result, soap, str)                                    \
@@ -32,6 +24,9 @@
     } while (0)
 
 void soap_perror(struct soap *soap, const char *str) {
+
+    //   if (soap->error)
+    // soap_print_fault(soap, stderr);
     if (NULL == str) {
         SOAP_DBGERR("[soap] error: %d, %s, %s\n", soap->error,
                     *soap_faultcode(soap), *soap_faultstring(soap));
@@ -42,135 +37,121 @@ void soap_perror(struct soap *soap, const char *str) {
     return;
 }
 
-void *ONVIF_soap_malloc(struct soap *soap, unsigned int n) {
-    void *p = NULL;
+#define SOAP_TO "urn:schemas-xmlsoap-org:ws:2005:04:discovery"
+#define SOAP_ACTION "http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe"
+#define SOAP_MCAST_ADDR "soap.udp://239.255.255.250:3702" // onvif规定的组播地址
+#define SOAP_ITEM ""                                   // 寻找的设备范围
+#define SOAP_COMPAT_TYPES "dn:NetworkVideoTransmitter" // 寻找的设备类型
 
-    if (n > 0) {
-        p = soap_malloc(soap, n);
-        SOAP_ASSERT(NULL != p);
-        memset(p, 0x00, n);
-    }
-    return p;
-}
+class OnvifSoap {
+public:
+    OnvifSoap(int timeout) {
+        // There is no need to call soap_init to initialize the context
+        // allocated with soap_new, since soap_new initializes the allocated
+        // context.
+        soap_ = soap_new();
 
-struct soap *ONVIF_soap_new(int timeout) {
-    struct soap *soap = NULL; // soap环境变量
+        soap_set_namespaces(soap_, namespaces); // 设置soap的namespaces
 
-    SOAP_ASSERT(NULL != (soap = soap_new()));
+        // 不正常数据设置成20s
+        if (timeout <= 0)
+            timeout = 20;
 
-    soap_set_namespaces(soap, namespaces); // 设置soap的namespaces
-    soap->recv_timeout = timeout; // 设置超时（超过指定时间没有数据就退出）
-    soap->send_timeout = timeout;
-    soap->connect_timeout = timeout;
+        soap_->recv_timeout = timeout; // 设置超时（超过指定时间没有数据就退出）
+        soap_->send_timeout = timeout;
+        soap_->connect_timeout = timeout;
 
 #if defined(__linux__) ||                                                      \
     defined(__linux) // 参考https://www.genivia.com/dev.html#client-c的修改：
-    soap->socket_flags = MSG_NOSIGNAL; // To prevent connection reset errors
+        soap_->socket_flags =
+            MSG_NOSIGNAL; // To prevent connection reset errors
 #endif
 
-    soap_set_mode(soap,
-                  SOAP_C_UTFSTRING); // 设置为UTF-8编码，否则叠加中文OSD会乱码
+        soap_set_mode(
+            soap_,
+            SOAP_C_UTFSTRING); // 设置为UTF-8编码，否则叠加中文OSD会乱码
+    }
+    ~OnvifSoap() {
+        soap_destroy(
+            soap_);      // deletes data, array, and other managed C++ objects
+        soap_end(soap_); // delete managed memory。soap_malloc
+        // soap_done(soap_); // Reset, close communications, and remove
+        // callbacks
+        soap_free(soap_); /* we're done with the context */
+    }
 
-    return soap;
-}
+    struct soap *soap() { return soap_; }
 
-void ONVIF_soap_delete(struct soap *soap) {
-    soap_destroy(soap); // remove deserialized class instances (C++ only)
-    soap_end(soap);  // Clean up deserialized data (except class instances) and
-                     // temporary data
-    soap_done(soap); // Reset, close communications, and remove callbacks
-    soap_free(soap); // Reset and deallocate the context created with soap_new
-                     // or soap_copy
-}
+    void *Malloc(size_t n) {
+        if (!n) {
+            return nullptr;
+        }
+        // Allocate a block of heap memory managed by the specified soap context
+        // All such blocks allocated are deleted with a single call to soap_end.
+        auto p = soap_malloc(soap_, n);
+        assert(p);
+        return p;
+    }
 
-/************************************************************************
-**函数：ONVIF_init_header
-**功能：初始化soap描述消息头
-**参数：
-        [in] soap - soap环境变量
-**返回：无
-**备注：
-    1).
-在本函数内部通过ONVIF_soap_malloc分配的内存，将在ONVIF_soap_delete中被释放
-************************************************************************/
-void ONVIF_init_header(struct soap *soap) {
-    struct SOAP_ENV__Header *header = NULL;
+    const char *WsaRandUuid() { return soap_wsa_rand_uuid(soap_); }
 
-    SOAP_ASSERT(NULL != soap);
+    void InitHeader() {
+        // T * soap_new_T(struct soap*) allocates and initializes data of type T
+        // in context-managed heap memory, managed data is deleted with
+        // soap_destroy (deletes C++ objects) and soap_end (deletes all other
+        // data), and you can also use soap_malloc to allocate uninitialized
+        // context-managed memory.
+        struct SOAP_ENV__Header *header = soap_new_SOAP_ENV__Header(soap_);
+        soap_default_SOAP_ENV__Header(soap_, header);
 
-    header = (struct SOAP_ENV__Header *)ONVIF_soap_malloc(
-        soap, sizeof(struct SOAP_ENV__Header));
-    soap_default_SOAP_ENV__Header(soap, header);
-    header->wsa__MessageID = (char *)soap_wsa_rand_uuid(soap);
-    header->wsa__To = (char *)ONVIF_soap_malloc(soap, strlen(SOAP_TO) + 1);
-    header->wsa__Action =
-        (char *)ONVIF_soap_malloc(soap, strlen(SOAP_ACTION) + 1);
-    strcpy(header->wsa__To, SOAP_TO);
-    strcpy(header->wsa__Action, SOAP_ACTION);
-    soap->header = header;
+        header->wsa__MessageID = (char *)this->WsaRandUuid();
+        header->wsa__To = (char *)this->Malloc(strlen(SOAP_TO) + 1);
+        header->wsa__Action = (char *)this->Malloc(strlen(SOAP_ACTION) + 1);
+        strcpy(header->wsa__To, SOAP_TO);
+        strcpy(header->wsa__Action, SOAP_ACTION);
+        soap_->header = header;
+    }
 
-    return;
-}
+    void InitProbeType(struct wsdd__ProbeType *probe) {
+        // 用于描述查找哪类的Web服务
+        struct wsdd__ScopesType *scope = soap_new_wsdd__ScopesType(soap_);
 
-/************************************************************************
-**函数：ONVIF_init_ProbeType
-**功能：初始化探测设备的范围和类型
-**参数：
-        [in]  soap  - soap环境变量
-        [out] probe - 填充要探测的设备范围和类型
-**返回：
-        0表明探测到，非0表明未探测到
-**备注：
-    1).
-在本函数内部通过ONVIF_soap_malloc分配的内存，将在ONVIF_soap_delete中被释放
-************************************************************************/
-void ONVIF_init_ProbeType(struct soap *soap, struct wsdd__ProbeType *probe) {
-    struct wsdd__ScopesType *scope = NULL; // 用于描述查找哪类的Web服务
+        soap_default_wsdd__ScopesType(soap_, scope); // 设置寻找设备的范围
+        scope->__item = "";
 
-    SOAP_ASSERT(NULL != soap);
-    SOAP_ASSERT(NULL != probe);
+        soap_default_wsdd__ProbeType(soap_, probe);
+        probe->Scopes = scope;
+        probe->Types = (char *)SOAP_COMPAT_TYPES; // 设置寻找设备的类型
+    }
 
-    scope = (struct wsdd__ScopesType *)ONVIF_soap_malloc(
-        soap, sizeof(struct wsdd__ScopesType));
-    soap_default_wsdd__ScopesType(soap, scope); // 设置寻找设备的范围
-    scope->__item = (char *)ONVIF_soap_malloc(soap, strlen(SOAP_ITEM) + 1);
-    strcpy(scope->__item, SOAP_ITEM);
+    int Error() { return soap_->error; }
 
-    memset(probe, 0x00, sizeof(struct wsdd__ProbeType));
-    soap_default_wsdd__ProbeType(soap, probe);
-    probe->Scopes = scope;
-    probe->Types = (char *)ONVIF_soap_malloc(soap, strlen(SOAP_TYPES) +
-                                                       1); // 设置寻找设备的类型
-    strcpy(probe->Types, SOAP_TYPES);
-
-    return;
-}
+private:
+    struct soap *soap_;
+};
 
 void ONVIF_DetectDevice(void (*cb)(char *DeviceXAddr)) {
     int i;
     int result = 0;
     unsigned int count = 0;          // 搜索到的设备个数
-    struct soap *soap = NULL;        // soap环境变量
     struct wsdd__ProbeType req;      // 用于发送Probe消息
     struct __wsdd__ProbeMatches rep; // 用于接收Probe应答
     struct wsdd__ProbeMatchType *probeMatch;
 
-    SOAP_ASSERT(NULL != (soap = ONVIF_soap_new(SOAP_SOCK_TIMEOUT)));
+    OnvifSoap onvif_soap(SOAP_SOCK_TIMEOUT);
+    onvif_soap.InitHeader();        // 设置消息头描述
+    onvif_soap.InitProbeType(&req); // 设置寻找的设备的范围和类型
 
-    ONVIF_init_header(soap);          // 设置消息头描述
-    ONVIF_init_ProbeType(soap, &req); // 设置寻找的设备的范围和类型
-    result = soap_send___wsdd__Probe(soap, SOAP_MCAST_ADDR, NULL,
+    result = soap_send___wsdd__Probe(onvif_soap.soap(), SOAP_MCAST_ADDR, NULL,
                                      &req); // 向组播地址广播Probe消息
     while (SOAP_OK == result) // 开始循环接收设备发送过来的消息
     {
-        memset(&rep, 0x00, sizeof(rep));
-        result = soap_recv___wsdd__ProbeMatches(soap, &rep);
+        soap_default___wsdd__ProbeMatches(onvif_soap.soap(), &rep);
+        result = soap_recv___wsdd__ProbeMatches(onvif_soap.soap(), &rep);
         if (SOAP_OK == result) {
-            if (soap->error) {
-                soap_perror(soap, "ProbeMatches");
+            if (onvif_soap.Error()) {
+                soap_perror(onvif_soap.soap(), "ProbeMatches");
             } else { // 成功接收到设备的应答消息
-                // dump__wsdd__ProbeMatches(&rep);
-
                 if (NULL != rep.wsdd__ProbeMatches) {
                     count += rep.wsdd__ProbeMatches->__sizeProbeMatch;
                     for (i = 0; i < rep.wsdd__ProbeMatches->__sizeProbeMatch;
@@ -178,48 +159,15 @@ void ONVIF_DetectDevice(void (*cb)(char *DeviceXAddr)) {
                         probeMatch = rep.wsdd__ProbeMatches->ProbeMatch + i;
                         std::cout << probeMatch->XAddrs << ", "
                                   << probeMatch->Types << std::endl;
-
-                        // {
-                        //     struct _tds__GetDeviceInformation devinfo_req;
-                        //     struct _tds__GetDeviceInformationResponse
-                        //         devinfo_resp;
-
-                        //     memset(&devinfo_req, 0x00, sizeof(devinfo_req));
-                        //     memset(&devinfo_resp, 0x00,
-                        //     sizeof(devinfo_resp)); result =
-                        //     soap_call___tds__GetDeviceInformation(
-                        //         soap, probeMatch->XAddrs, NULL, &devinfo_req,
-                        //         devinfo_resp);
-
-                        //     //
-                        //     dump_tds__GetDeviceInformationResponse(&devinfo_resp);
-                        //     printf("Manufacturer:%s\n",
-                        //            devinfo_resp.Manufacturer);
-                        //     printf("Model:%s\n", devinfo_resp.Model);
-                        //     printf("FirmwareVersion:%s\n",
-                        //            devinfo_resp.FirmwareVersion);
-                        //     printf("SerialNumber:%s\n",
-                        //            devinfo_resp.SerialNumber);
-                        //     printf("HardwareId:%s\n",
-                        //     devinfo_resp.HardwareId);
-                        // }
-                        // if (NULL != cb) {
-                        //     cb(probeMatch
-                        //            ->XAddrs); // 使用设备服务地址执行函数回调
-                        // }
                     }
                 }
             }
-        } else if (soap->error) {
+        } else if (onvif_soap.Error()) {
             break;
         }
     }
 
     SOAP_DBGLOG("\ndetect end! It has detected %d devices!\n", count);
-
-    if (NULL != soap) {
-        ONVIF_soap_delete(soap);
-    }
 
     return;
 }
